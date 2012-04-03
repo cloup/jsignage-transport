7a01428793e3249077343d863c65f251d8942d4f
Christophe Loup <cloup@freestudios.ch>
2012-04-03 10:40:30 +0200

    corrected departure time display

--- sample_geneva/js/sample.js
+++ sample_geneva/js/sample.js
@@ -10,16 +10,19 @@ $(function(){
 		
 		var dataArray = [];
 		$(data.stationboard).each(function () {
-			var prognosis, departure, delay, line = '<tr><td>';
+			var prognosis, delay, line = '';
+			var departureAll = this.stop.departure.split('T');
+			var departureTime = departureAll[1];
+			var departure = departureTime.split(':');
+			var departureString = departure[0] + ':' + departure[1];
 			if (this.stop.prognosis.time) {
 		        prognosis = this.stop.prognosis.time.split(':');
-		        departure = this.stop.departure.split(':');
 		        delay = (parseInt(prognosis[0]) * 60 + parseInt(prognosis[1])) - (parseInt(departure[0]) * 60 + parseInt(departure[1]));
-				dataArray.push($.textArea({ top: '80%', displayAlign: 'before', fontSize: 48, fontFamily: 'Helvetica, Arial' }).text($.tspan(this.stop.departure + ' ').tspan(delay + ' min',{fontWeight: 'bold'}).tbreak().tspan(this.name, {fill: '#666666'}).tbreak().tspan(this.to, {fill: '#666666', fontSize: 28})));
+				dataArray.push($.textArea({ top: '80%', displayAlign: 'before', fontSize: 48, fontFamily: 'Helvetica, Arial' }).text($.tspan(departureString + ' ').tspan(delay + ' min',{fontWeight: 'bold'}).tbreak().tspan(this.name, {fill: '#666666'}).tbreak().tspan(this.to, {fill: '#666666', fontSize: 28})));
 		    }
 			else
 			{
-				dataArray.push($.textArea({ top: '80%', displayAlign: 'before', fontSize: 48, fontFamily: 'Helvetica, Arial' }).text($.tspan(this.stop.departure).tbreak().tspan(this.name, {fill: '#666666'}).tbreak().tspan(this.to, {fill: '#666666', fontSize: 28})));
+				dataArray.push($.textArea({ top: '80%', displayAlign: 'before', fontSize: 48, fontFamily: 'Helvetica, Arial' }).text($.tspan(departureString).tbreak().tspan(this.name, {fill: '#666666'}).tbreak().tspan(this.to, {fill: '#666666', fontSize: 28})));
 		    }
 		});
 		
